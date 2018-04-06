81da176cb71179062d5a392dd135963afa662adf
chtu <tuttle.charlie@gmail.com>
2018-04-06 01:36:32 -0700

    Managed to get the FirebaseAuth working! Doesn't work with fb yet

--- Petworq/app/src/main/java/com/petworq/petworq/MainActivity.java
+++ Petworq/app/src/main/java/com/petworq/petworq/MainActivity.java
@@ -1,11 +1,29 @@
 package com.petworq.petworq;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,31 +33,238 @@ import com.facebook.appevents.AppEventsLogger;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "MainActivity";
 
-    List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+    private Button mSignInButton;
+    private Button mSignOutButton;
+    private TextView mStatusTextView;
+
+    private FirebaseUser user;
+
+    List<AuthUI.IdpConfig> providers;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()
+        );
+
+        mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+        mSignInButton.setOnClickListener(this);
+        mSignOutButton.setOnClickListener(this);
+
+        updateUI();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateUI();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.sign_in_button):
+                signIn();
+                break;
+            case (R.id.sign_out_button):
+                signOut();
+                break;
+        }
+    }
+
+    private void signIn() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+                RC_SIGN_IN
+        );
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+            public void onComplete(@NonNull Task<Void> task) {
+
+            }
+        });
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        updateUI();
+    }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                // Sign in failed, check response for error code
+                Log.d(TAG, "onActivityResult:" + resultCode);
+            }
+        }
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (user == null) {
+            mStatusTextView.setText("You are not signed in.");
+            mSignInButton.setVisibility(View.VISIBLE);
+            mSignOutButton.setVisibility(View.GONE);
+        } else {
+            mStatusTextView.setText(generateSignInMessage(user));
+            mSignInButton.setVisibility(View.GONE);
+            mSignOutButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private String generateSignInMessage(FirebaseUser user) {
+        String outgoingMessage = "You are signed in as " + user.getDisplayName() + "\n";
+        switch (user.getProviders().get(0)) {
+            case ("google.com"):
+                outgoingMessage += "via Google!";
+                break;
+            case ("password"):
+                outgoingMessage += "via your email, " + user.getEmail();
+                break;
+            case ("facebook.com"):
+                outgoingMessage += "via Facebook!";
+                break;
+        }
+        return outgoingMessage;
+    }
+
+    /*
+    SignInButton signInButton;
+    Button signOutButton;
+    TextView status;
+    TextView statusTextView;
+    GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleSignInAccount mGoogleSignInAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-        mAuth = FirebaseAuth.getInstance();
+        // Link the elements with our variables
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signOutButton = (Button) findViewById(R.id.sign_out_button);
+        statusTextView = (TextView) findViewById(R.id.status_text_view);
+        status = (TextView) findViewById(R.id.status);
+
+        signInButton.setOnClickListener(this);
+        signOutButton.setOnClickListener(this);
+
+        updateUI();
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
     }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                statusTextView.setText("Signed out");
+            }
+        });
+
+        updateUI();
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Log.d(TAG, "onActivityResult requestCode:" + requestCode);
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+
+        updateUI();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            statusTextView.setText("Hello " + acct.getDisplayName());
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
+    }
+
+    private void updateStatus() {
+        if (mGoogleSignInAccount != null) {
+            status.setText("You are signed in as " + mGoogleSignInAccount.getDisplayName());
+        }
+    }
+
+    private void updateUI() {
+        mGoogleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (mGoogleSignInAccount == null) {
+            status.setText("You are not signed in");
+            signInButton.setVisibility(View.VISIBLE);
+            signOutButton.setVisibility(View.INVISIBLE);
+        } else {
+            status.setText("You are signed in as " + mGoogleSignInAccount.getDisplayName());
+            signInButton.setVisibility(View.INVISIBLE);
+            signOutButton.setVisibility(View.VISIBLE);
+        }
+    }
+    */
 }
