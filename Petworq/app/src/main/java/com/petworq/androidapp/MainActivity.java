3d0aa93eda62d62f255f68551f32bfe5a0cb7a9a
chtu <tuttle.charlie@gmail.com>
2018-04-13 15:29:23 -0700

    Removed files not relevant to new framework

--- Petworq/app/src/main/java/com/petworq/androidapp/MainActivity.java
+++ Petworq/app/src/main/java/com/petworq/androidapp/MainActivity.java
@@ -2,24 +2,24 @@ package com.petworq.androidapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.ViewGroup;
 
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Router;
+import com.bluelinelabs.conductor.RouterTransaction;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.petworq.androidapp.Authentication.AuthActivity;
-import com.petworq.androidapp.Authentication.SignInFragment;
-import com.petworq.androidapp.ContentFragments.BaseOptionsFragment;
-import com.petworq.androidapp.ToolbarFragments.UnauthenticatedToolbarFragment;
-import com.petworq.androidapp.ToolbarFragments.ToolbarFragment;
 import com.petworq.androidapp.UtilityClasses.AuthUtil;
-import com.petworq.androidapp.UtilityClasses.DataUtil;
-import com.petworq.androidapp.UtilityClasses.FragmentUtil;
+import com.petworq.androidapp.controllers.SignInController;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
 
@@ -33,6 +33,11 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
     private static final int RC_SIGN_IN = 111;
     private static final int RC_STORE_USER_INFO = 222;
 
+    private Router mRouter;
+
+    @BindView(R.id.controller_container)
+    ViewGroup container;
+
     private FirebaseAuth mAuth;
     private DocumentReference mUserDocRef;
 
@@ -41,11 +46,25 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        mRouter = Conductor.attachRouter(this, container, savedInstanceState);
+
+        if (!mRouter.hasRootController()) {
+            mRouter.setRoot(RouterTransaction.with(new SignInController()));
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.sign_in);
+        setSupportActionBar(toolbar);
+
+
+
         // Set up the AuthStateListener
         mAuth = FirebaseAuth.getInstance();
         mAuth.addAuthStateListener(this);
 
-        initializePage();
+        //initializePage();
 
         if (DEBUG) {
             // If DEBUG is true, the user will be signed out at the beginning of the session.
@@ -59,11 +78,18 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
+        /*
 
         if (AuthUtil.getUser() == null) {
-            UnauthenticatedToolbarFragment newToolbarFragment = new UnauthenticatedToolbarFragment();
+            UnauthToolbarController newToolbarFragment = new UnauthToolbarController();
             SignInFragment newContentFragment = new SignInFragment();
 
             FragmentUtil.updateMainUi(newToolbarFragment, newContentFragment, this);
@@ -72,18 +98,21 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
             ToolbarFragment newToolbarFragment = new ToolbarFragment();
             FragmentUtil.updateToolBar(newToolbarFragment, this);
         }
+        */
     }
 
     @Override
     public void onAuthStateChanged(@NonNull FirebaseAuth auth) {
+        /*
         FirebaseUser user = auth.getCurrentUser();
 
         if (user == null) {
             Log.d(TAG, "Changing layout to sign in page");
-            UnauthenticatedToolbarFragment newToolbarFragment = new UnauthenticatedToolbarFragment();
+            UnauthToolbarController newToolbarFragment = new UnauthToolbarController();
             SignInFragment newContentFragment = new SignInFragment();
             FragmentUtil.updateMainUi(newToolbarFragment, newContentFragment, this);
         }
+        */
     }
 
 
@@ -99,13 +128,13 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
         }
     }
 
-
+/*
     private void initializePage() {
         if (!AuthUtil.userIsSignedIn()) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-            UnauthenticatedToolbarFragment notAuthToolbarFragment = new UnauthenticatedToolbarFragment();
+            UnauthToolbarController notAuthToolbarFragment = new UnauthToolbarController();
             SignInFragment signInFragment = new SignInFragment();
 
             fragmentTransaction.add(R.id.fragment_toolbar, notAuthToolbarFragment);
@@ -117,21 +146,12 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
             }
         }
     }
+    */
 
     @Override
     public void onBackPressed() {
-        if (BACK_ALLOWED) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-
-            if (fragment != null && fragmentManager.getBackStackEntryCount() > 0) {
-                if (FragmentUtil.isMainFragment(fragment)) {
-                    fragmentManager.beginTransaction().detach(fragment).commit();
-                    super.onBackPressed();
-                }
-            }
-        } else {
-            moveTaskToBack(false);
+        if (!mRouter.handleBack()) {
+            super.onBackPressed();
         }
     }
 }
---
+++ Petworq/app/src/main/java/com/petworq/androidapp/controllers/SignInController.java
@@ -0,0 +1,42 @@
+package com.petworq.androidapp.controllers;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.bluelinelabs.conductor.Controller;
+import com.petworq.androidapp.Authentication.AuthActivity;
+import com.petworq.androidapp.R;
+
+public class SignInController extends Controller implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 123;
+
+    private Button mSignInButton;
+
+    public SignInController() {}
+
+
+    @Override
+    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
+        View view = inflater.inflate(R.layout.view_sign_in, container, false);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case(R.id.sign_in_button):
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.startActivity(new Intent(activity, AuthActivity.class));
+                break;
+        }
+    }
+}
